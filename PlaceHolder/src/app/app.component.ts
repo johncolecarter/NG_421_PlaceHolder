f33c1075c00c1f6081755357818d8ce03d18853c
johncolecarter <colecarter7777@gmail.com>
2020-05-04 20:28:00 -0500

    added filtering

--- PlaceHolder/src/app/app.component.ts
+++ PlaceHolder/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ITodo } from './interfaces/itodo';
 import { MatTableDataSource } from '@angular/material/table';
 import { TodoService } from './services/todo.service';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +16,23 @@ export class AppComponent implements OnInit {
   displayedColumns: string[] = ['id', 'userId', 'title', 'completed'];
   dataSource: MatTableDataSource<ITodo>;
 
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   constructor(private todoService: TodoService) { }
 
   async ngOnInit() {
     const data = await this.todoService.get();
     this.dataSource = new MatTableDataSource(data);
+
+    this.dataSource.sort = this.sort;
+
+    this.dataSource.paginator = this.paginator;
   }
 }
